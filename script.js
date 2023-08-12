d552e94e8689b80b343028afe8546703bd5987fb
Franco Patti <francopattialvarez@gmail.com>
2023-08-12 05:49:35 -0300

    trivia de juego de tronos

--- script.js
+++ script.js
@@ -30,4 +30,65 @@ function comenzarJuego(){
     document.getElementById("pantalla-juego").style.display = "block";
     cargarPregunta();
 }
+//funcion que carga la siguiente pregunta y sus opciones
+function cargarPregunta(){
+    //controlo si se acabaron las preguntas
+    if(preguntas.length <= posActual){
+        terminarJuego();
+    }
+    else{//cargo las opciones
+        //limpio las clases que se asignaron
+        limpiarOpciones();
 
+
+        document.getElementById("preguntaX").innerHTML = preguntas[posActual];
+        document.getElementById("n0").innerHTML = opciones[posActual][0];
+        document.getElementById("n1").innerHTML = opciones[posActual][1];
+        document.getElementById("n2").innerHTML = opciones[posActual][2];
+    }
+}
+
+function limpiarOpciones(){
+    document.getElementById("n0").className = "nombre";
+    document.getElementById("n1").className = "nombre";
+    document.getElementById("n2").className = "nombre";
+
+    document.getElementById("l0").className = "letra";
+    document.getElementById("l1").className = "letra";
+    document.getElementById("l2").className = "letra";
+}
+
+function comprobarRespuesta(opElegida){
+    if(opElegida==correctas[posActual]){//acerta
+        //agrego las clases para poner el color verde a la opcion elegida
+        document.getElementById("n" + opElegida).className = "nombre nombreAcertado";
+        document.getElementById("l" + opElegida).className = "letra letraAcertado";
+        cantidadAcertadas++;
+    }else{//no acerta
+        //agrego las clases para poner el color rojo a la opcion elegida
+        document.getElementById("n" + opElegida).className = "nombre nombreNoAcertado";
+        document.getElementById("l" + opElegida).className = "letra letraNoAcertado";
+
+        //opcion que era correcta
+        document.getElementById("n" + correctas[posActual]).className = "nombre nombreAcertado";
+        document.getElementById("l" + correctas[posActual]).className = "letra letraAcertado";
+    }
+    posActual++;
+    //espera 1 seg y pasa a mostrar la siguiente pregunta con sus opciones
+    setTimeout(cargarPregunta,1000);
+}
+function terminarJuego(){
+    //se ocultan las pantallas y se muestra la pantalla final
+    document.getElementById("pantalla-juego").style.display = "none";
+    document.getElementById("pantalla-final").style.display = "block";
+    //agrego los resultados
+    document.getElementById("numCorrectas").innerHTML = cantidadAcertadas;
+    document.getElementById("numInCorrectas").innerHTML = preguntas.length - cantidadAcertadas;
+}
+
+function volverAlInicio(){
+    //se oculta la pantalla y se activa la inicial
+    document.getElementById("pantalla-final").style.display = "none";
+    document.getElementById("pantalla-juego").style.display = "none";
+    document.getElementById("pantalla-inicial").style.display = "block";
+}
